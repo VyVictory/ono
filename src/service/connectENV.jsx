58a1a826dc5952b767f1cdaa30cd3cc2cf866758
VyVictory <145247364+VyVictory@users.noreply.github.com>
2025-03-06 22:25:47 +0700

    Update connectENV.jsx

--- src/service/connectENV.jsx
+++ src/service/connectENV.jsx
@@ -1,7 +1,7 @@
 // let uri = "https://doan4-5by5.onrender.com/";
 // let uri = "http://localhost:3001/";
 //https://doan4-5by5.onrender.com/
-let uri = "https://onobackend.vercel.app/";
+let uri = "https://ono-wtxp.onrender.com/";
 const apiAuth = uri + "auth";
 const api = uri + "";
 const apiUser = uri + "user";
