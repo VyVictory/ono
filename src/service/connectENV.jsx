e2a14fbcc0c7c46a24d1ee0e62d1631dbd588973
VyVictory <145247364+VyVictory@users.noreply.github.com>
2025-03-07 13:11:32 +0700

    a

--- src/service/connectENV.jsx
+++ src/service/connectENV.jsx
@@ -1,11 +1,9 @@
 // let uri = "https://doan4-5by5.onrender.com/";
 // let uri = "http://localhost:3001/";
-//https://doan4-5by5.onrender.com/
-let uri = "https://ono-wtxp.onrender.com/";
-const apiAuth = uri + "auth";
-const api = uri + "";
-const apiUser = uri + "user";
-
+const uri = process.env.REACT_APP_API_URL || "http://localhost:3001/";
+const apiAuth = `${uri}auth`;
+const api = `${uri}`;
+const apiUser = `${uri}user`;
 export default {
   apiAuth,
   api,
