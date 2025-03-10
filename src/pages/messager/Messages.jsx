72ac594208b7cf73a27bfb8a1bbe6416121b62cd
VyVictory <145247364+VyVictory@users.noreply.github.com>
2025-03-11 03:15:14 +0700

    Update Messages.jsx

--- src/pages/messager/Messages.jsx
+++ src/pages/messager/Messages.jsx
@@ -14,17 +14,6 @@ const Messages = () => {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
-  useEffect(() => {
-    // Kiểm tra nếu trang đã được reload chưa
-    const hasReloaded = sessionStorage.getItem('hasReloaded');
-
-    if (!hasReloaded) {
-      sessionStorage.setItem('hasReloaded', 'true');
-      window.location.reload();
-    } else {
-      sessionStorage.removeItem('hasReloaded'); // Để khi vào lại sẽ reload
-    }
-  }, []);
   const [isSidebarOpen, setSidebarOpen] = useState(false);
   const [isRightbarOpen, setRightbarOpen] = useState(false);
   const [isRightbarOpen1, setRightbarOpen1] = useState(true);
