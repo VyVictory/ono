bd3aa2d0be2d4a10f0c3723f9b2f33fa932df80d
VyVictory <145247364+VyVictory@users.noreply.github.com>
2025-03-11 01:10:43 +0700

    vv

--- src/service/connectENV.jsx
+++ src/service/connectENV.jsx
@@ -1,5 +1,6 @@
 // let uri = "https://doan4-5by5.onrender.com/";
-// let uri = "http://localhost:3001/";
+// let uri = "http://localhost:3001/"; 
+
 const uri = process.env.REACT_APP_DATABASE_URL || "http://localhost:3001/";
 const apiAuth = `${uri}auth`;
 const api = `${uri}`;
