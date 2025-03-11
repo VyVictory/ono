f483a039a868a75cb60a61de9186e51e22211804
VyVictory <145247364+VyVictory@users.noreply.github.com>
2025-03-11 18:28:58 +0700

    Update PostProfile.jsx

--- src/pages/profile/post/PostProfile.jsx
+++ src/pages/profile/post/PostProfile.jsx
@@ -17,45 +17,10 @@ const PostProfile = ({ data }) => {
   const [isPassed, setIsPassed] = useState(false);
   const [height, setHeight] = useState(0);
   // Cập nhật chiều cao
-  useEffect(() => {
-    const observer = new ResizeObserver(() => {
-      if (targetRefH.current) {
-        setHeight(targetRefH.current.offsetHeight);
-      }
-    });
-    // console.log("dawdaw");
-    targetRefH.current && observer.observe(targetRefH.current);
-    return () => observer.disconnect();
-  }, []);
-  // Cuộn đến phần tử khi render
-  useEffect(() => {
-    scrollRef.current?.scrollIntoView({ behavior: "smooth" });
-  }, []);
-  const violetRef = useRef(null);
-  const [heightBG, setHeightBG] = useState(0);
-
-  useEffect(() => {
-    if (violetRef.current) {
-      setHeightBG(violetRef.current.offsetHeight);
-    }
-  }, []);
-  // Xử lý sự kiện cuộn
-  const handleScroll = useCallback(() => {
-    if (targetRef.current && height != 0) {
-      const rect = targetRef.current.getBoundingClientRect();
-      setIsPassed(rect.bottom - window.innerHeight < 0);
-    }
-  }, [height]);
-
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-    handleScroll();
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, [handleScroll]);
+ 
   return (
     <div
-      className="relative  flex" //bg-black
-      style={{ minHeight: heightBG }}
+      className="relative  flex" //bg-black 
     >
       {/* left */}
      
