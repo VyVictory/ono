d6032c7f056043c29accf38418c67441596e8207
VyVictory <145247364+VyVictory@users.noreply.github.com>
2025-03-11 18:30:02 +0700

    Update PostProfile.jsx

--- src/pages/profile/post/PostProfile.jsx
+++ src/pages/profile/post/PostProfile.jsx
@@ -17,13 +17,83 @@ const PostProfile = ({ data }) => {
   const [isPassed, setIsPassed] = useState(false);
   const [height, setHeight] = useState(0);
   // Cập nhật chiều cao
- 
+  useEffect(() => {
+    const observer = new ResizeObserver(() => {
+      if (targetRefH.current) {
+        setHeight(targetRefH.current.offsetHeight);
+      }
+    });
+    // console.log("dawdaw");
+    targetRefH.current && observer.observe(targetRefH.current);
+    return () => observer.disconnect();
+  }, []);
+  // Cuộn đến phần tử khi render
+  useEffect(() => {
+    scrollRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, []);
+  const violetRef = useRef(null);
+  const [heightBG, setHeightBG] = useState(0);
+
+  useEffect(() => {
+    if (violetRef.current) {
+      setHeightBG(violetRef.current.offsetHeight);
+    }
+  }, []);
+  // Xử lý sự kiện cuộn
+  const handleScroll = useCallback(() => {
+    if (targetRef.current && height != 0) {
+      const rect = targetRef.current.getBoundingClientRect();
+      setIsPassed(rect.bottom - window.innerHeight < 0);
+    }
+  }, [height]);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    handleScroll();
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [handleScroll]);
   return (
     <div
-      className="relative  flex" //bg-black 
+      className="relative  flex" //bg-black
+      style={{ minHeight: heightBG }}
     >
       {/* left */}
-     
+      <div className={`h-full  w-full min-h-screen  flex justify-center `}>
+        <div
+          ref={targetRefH}
+          className={`pb-4  hidden  w-full md:flex profileW   flex-row ${
+            isPassed ? "fixed bottom-0 px-3" : ""
+          }`}
+        >
+          <div className="flex flex-col  w-2/5 ">
+            <PostLeft data={data} />
+          </div>
+          <div className="  min-h-screen w-3/5 hidden md:block"></div>
+        </div>
+      </div>
+      <div className="absolute">
+        <div className="bg-violet-600 " style={{ height: `${height}px` }}></div>
+        <div className="w-full" ref={targetRef}></div>
+      </div>
+      {/* right */}
+      <div
+        ref={violetRef}
+        className="absolute  pb-2"
+        style={{ pointerEvents: "none" }}
+      >
+        <div className="  w-full flex profileW justify-center md:pl-6">
+          <div className="flex flex-row justify-center">
+            {/* // độn div left */}
+            <div className="  min-h-screen w-2/5 hidden md:block"></div>
+            <div
+              className="flex flex-col space-y-3   md:w-3/5 md:px-0"
+              style={{ pointerEvents: "auto" }}
+            >
+              <PostRight />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
