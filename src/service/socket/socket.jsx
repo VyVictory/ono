1276fbc7d37019c953316fadad80b6867ed27271
VyVictory <145247364+VyVictory@users.noreply.github.com>
2025-03-19 18:04:34 +0700

    Update socket.jsx

--- src/service/socket/socket.jsx
+++ src/service/socket/socket.jsx
@@ -4,7 +4,6 @@ import { io } from "socket.io-client";
 const SOCKET_URL = "http://localhost:3001"; // Thay đổi thành URL server của bạn
 export const useSocket = (userId) => {
   const [socket, setSocket] = useState(null);
-  const isConnectRef = useRef(sessionStorage.getItem("isConnect") === "true");
   useEffect(() => {
     if (!userId) return;
 
@@ -19,17 +18,12 @@ export const useSocket = (userId) => {
       sessionStorage.setItem("isConnect", "true"); // 🔹 Lưu vào sessionStorage
       newSocket.emit("authenticate", userId); // 🔹 Gửi userId lên server
     });
-    newSocket.on("disconnect", () => {
-      console.log("Disconnected from socket");
-      isConnectRef.current = false;
-      sessionStorage.setItem("isConnect", "false"); // 🔹 Cập nhật sessionStorage khi ngắt kết nối
-    });
+    
     setSocket(newSocket);
 
     return () => {
       newSocket.disconnect();
-      isConnectRef.current = false;
-      sessionStorage.setItem("isConnect", "false"); // 🔹 Reset trạng thái khi unmount
+       // 🔹 Reset trạng thái khi unmount
     };
   }, [userId]);
 
