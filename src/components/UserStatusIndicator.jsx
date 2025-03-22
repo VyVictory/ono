d815c0656563822f908d75d1305c3b0b68a633e3
VyVictory <145247364+VyVictory@users.noreply.github.com>
2025-03-22 22:44:58 +0700

    Update UserStatusIndicator.jsx

--- src/components/UserStatusIndicator.jsx
+++ src/components/UserStatusIndicator.jsx
@@ -9,14 +9,41 @@ const UserStatusIndicator = ({ userId, userData }) => {
 
   // console.log(`User ${userId} is online:`, isOnline); // ✅ Kiểm tra log đã đúng chưa
 
+  // const StyledBadge = styled(Badge)(({ theme }) => ({
+  //   "& .MuiBadge-badge": {
+  //     backgroundColor: isOnline ? "#44b700" : "#707070",
+  //     color: isOnline ? "#44b700" : "#707070",
+  //     boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
+  //   },
+  // }));
   const StyledBadge = styled(Badge)(({ theme }) => ({
-    "& .MuiBadge-badge": {
+    '& .MuiBadge-badge': {
       backgroundColor: isOnline ? "#44b700" : "#707070",
       color: isOnline ? "#44b700" : "#707070",
       boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
+      '&::after': isOnline&&{
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%',
+        borderRadius: '50%',
+        animation: 'ripple 1.2s infinite ease-in-out',
+        border: '1px solid currentColor',
+        content: '""',
+      },
+    },
+    '@keyframes ripple': {
+      '0%': {
+        transform: 'scale(.8)',
+        opacity: 1,
+      },
+      '100%': {
+        transform: 'scale(2.4)',
+        opacity: 0,
+      },
     },
   }));
-
   return (
     <StyledBadge
       overlap="circular"
