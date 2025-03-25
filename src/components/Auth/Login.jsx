655da604e7bf11f8837d1e29336bbfe0e542de34
VyVictory <145247364+VyVictory@users.noreply.github.com>
2025-03-25 07:24:07 +0700

    Update Login.jsx

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -233,7 +233,7 @@ export default function Login({ chaneform }) {
                 type="button"
                 onClick={handleSubmit}
                 disabled={loading}
-                class="text-white w-full py-3 bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 text-center me-2 mb-2"
+                className="text-white w-full py-3 bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 text-center me-2 mb-2"
               >
                 Đăng nhập
               </button>
