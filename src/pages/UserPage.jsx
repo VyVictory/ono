3dc93fb7b77c5b46d8086b8800d496ed03e9f5da
VyVictory <145247364+VyVictory@users.noreply.github.com>
2025-03-25 07:27:27 +0700

    Update UserPage.jsx

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -35,7 +35,7 @@ const UserPage = () => {
 
   return (
     <div className="flex py-4 flex-col items-center h-screen overflow-auto">
-      <div className="max-w-[800px] px-3 md:px-0 space-y-3 w-full">
+      <div className="max-w-[1000px] px-3 xl:px-0  space-y-3 w-full">
         <HeadCreatePost />
         <div className="space-y-4 min-w-full">
           {isLoading ? (
