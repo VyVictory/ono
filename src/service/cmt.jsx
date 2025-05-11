2e86d23fd3e9d75f9bd88f005923acb5cd7152fd
VyVictory <145247364+VyVictory@users.noreply.github.com>
2025-05-11 14:16:48 +0700

    thân thiện người dùng

---
+++ src/service/cmt.jsx
@@ -0,0 +1,61 @@
+import api from "./components/api";
+import authToken from "./storage/authToken";
+import { nextLogin, nextError } from "./components/nextLogin";
+
+export const PostComment = async ({
+  postId,
+  content,
+  files,
+  video,
+  idCmt = null,
+}) => {
+  try {
+    const token = authToken.getToken();
+    if (!token) {
+      nextLogin();
+      return null;
+    }
+    
+    if (!content && (!files || files.length === 0) && !video) return null; 
+    const formData = new FormData();
+
+    formData.append("content", content?.trim() || "");
+
+    if (idCmt) {
+      formData.append("idCmt", idCmt);
+    }
+
+    if (files?.length) {
+      files.forEach((file) => {
+        formData.append("media", file);
+      });
+    }
+
+    if (video) {
+      formData.append("media", video); // Nếu video là 1 File object
+    }
+ 
+    const response = await api.post(`/cmt/${postId}/comment`, formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+
+    return response.data;
+  } catch (error) {
+    nextError(error);
+    return null;
+  }
+};
+
+export const getCmt = async (start, limit) => {
+  try {
+    const response = await api.get(
+      `/post/postByRange/?start=${start}&limit=${limit}`
+    );
+    return response.data;
+  } catch (error) {
+    // nextError(error);
+    return null;
+  }
+};
